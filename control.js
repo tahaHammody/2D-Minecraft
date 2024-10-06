6f3200c1dc6748ed2e1ac436c751cd4eaf9ad41e
t1s2h <t1.s2.h3.21@gmail.com>
2024-10-06 14:00:01 +0300

    main page

---
+++ control.js
@@ -0,0 +1,10 @@
+document.getElementById('single-player').addEventListener('click', function() {
+    window.location.href = 'game.html'; 
+});
+document.getElementById('minecraft-wiki').addEventListener('click', function() {
+    window.open('https://minecraft.fandom.com/wiki/Minecraft_Wiki', '_blank'); 
+
+});
+document.getElementById('quit').addEventListener('click', function() {
+    window.close(); 
+});
